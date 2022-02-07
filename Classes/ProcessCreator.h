07837f844531108595c2d8fd4c498a0f57bf9d4a
Santoshjonnakuti <santoshjonnakuti@gmail.com>
2022-02-07 21:46:37 +0530

    Final Commit

--- Classes/ProcessCreator.h
+++ Classes/ProcessCreator.h
@@ -35,8 +35,8 @@ public:
             preAT = rand();
             preBT = rand();
             PD.processId = i + 1;
-            PD.arrivalTime = 1 + preAT % 5;
-            PD.burstTime = 1 + preBT % 5;
+            PD.arrivalTime = 1 + preAT % 500;
+            PD.burstTime = 1 + preBT % 20;
             PD.burstTime1 = PD.burstTime;
             Processes[i] = new Process(PD);
             prevAT = PD.arrivalTime;
